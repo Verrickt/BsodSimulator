1155851bc02ae327125d6561322fee4e9ec0c254
Verrickt <Verrickt@users.noreply.github.com>
2017-09-23 19:48:54 +0800

    Rename

---
+++ BsodSimulator/Converters/GenericObjectConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace BsodSimulator.Converters
+{
+    /// <summary>
+    /// A converter that makes compiler happy
+    /// </summary>
+    public class GenericObjectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
