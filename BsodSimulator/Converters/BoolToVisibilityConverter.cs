3683fa5634a88141e4d5e5b5f07143ccf263cf85
Verrickt <Verrickt@users.noreply.github.com>
2017-09-23 19:49:20 +0800

    Add bool to visibility converter

---
+++ BsodSimulator/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace BsodSimulator.Converters
+{
+    public class BoolToVisibilityConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool b)
+            {
+                return b ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
